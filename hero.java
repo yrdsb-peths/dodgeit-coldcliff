cee2bd8e2130af525c72467bae470b5068c7c55b
coldcliff <fangclifford7@gmail.com>
2024-04-22 12:30:41 -0400

    Add hero

---
+++ hero.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class hero extends Actor
+{
+    boolean atTop = true;
+    public void act()
+    {
+         if(Greenfoot.mouseClicked(null)) {
+            atTop =!atTop;
+         }
+         if(atTop){
+             setLocation(100,100);
+         } else {
+             setLocation(300,100);
+         }
+    }
+}
